dcc74b7459049a2b68d37f595dfc8de4245a7606
mimimi1123 <mimimi20021123@gmail.com>
2026-01-10 18:35:24 +0900

    割り込みに変更

--- second_semester/PID/1213/encoder.py
+++ second_semester/PID/1213/encoder.py
@@ -6,8 +6,7 @@ class Encoder:
         self.pin_a = pin_a
         self.pin_b = pin_b
         self.count = 0
-        self.last_a = 0
-        self.last_b = 0
+        self.last_state = 0
         
         # GPIO.setmodeは呼び出し元で行う前提だが、
         # クラス内で安全のためsetup前に現在のモードを確認することも可能。
@@ -15,41 +14,29 @@ class Encoder:
         GPIO.setup(self.pin_a, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.pin_b, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         
-        # 初期状態を読み取る
-        self.last_a = GPIO.input(self.pin_a)
-        self.last_b = GPIO.input(self.pin_b)
+        # 初期状態を2bitで保持 (A:bit1, B:bit0)
+        self.last_state = (GPIO.input(self.pin_a) << 1) | GPIO.input(self.pin_b)
         
-        GPIO.add_event_detect(self.pin_a, GPIO.BOTH, callback=self.update_count)
-        GPIO.add_event_detect(self.pin_b, GPIO.BOTH, callback=self.update_count)
+        # 双方の立ち上がり/立ち下がりを割り込みで監視
+        GPIO.add_event_detect(self.pin_a, GPIO.BOTH, callback=self.update_count, bouncetime=1)
+        GPIO.add_event_detect(self.pin_b, GPIO.BOTH, callback=self.update_count, bouncetime=1)
 
     def update_count(self, channel):
         state_a = GPIO.input(self.pin_a)
         state_b = GPIO.input(self.pin_b)
-        
-        # グレイコード方式でエンコーダの回転方向を判定
-        if self.last_a == 0 and self.last_b == 0:
-            if state_a == 1:
-                self.count += 1
-            elif state_b == 1:
-                self.count -= 1
-        elif self.last_a == 1 and self.last_b == 0:
-            if state_b == 1:
-                self.count += 1
-            elif state_a == 0:
-                self.count -= 1
-        elif self.last_a == 1 and self.last_b == 1:
-            if state_a == 0:
-                self.count += 1
-            elif state_b == 0:
-                self.count -= 1
-        elif self.last_a == 0 and self.last_b == 1:
-            if state_b == 0:
-                self.count += 1
-            elif state_a == 1:
-                self.count -= 1
-        
-        self.last_a = state_a
-        self.last_b = state_b
+        state = (state_a << 1) | state_b
+
+        # グレイコードの遷移表 (前状態->現状態で+1/-1/0を与える)
+        transition = {
+            0b00: {0b01: 1, 0b10: -1},
+            0b01: {0b11: 1, 0b00: -1},
+            0b11: {0b10: 1, 0b01: -1},
+            0b10: {0b00: 1, 0b11: -1},
+        }
+
+        delta = transition.get(self.last_state, {}).get(state, 0)
+        self.count += delta
+        self.last_state = state
 
     def get_count(self):
         return self.count
